ec56d66acf5dd4b81bce1dc021ffce9ea9364ed3
Tom Pivin <tommy.pivin94@gmail.com>
2023-03-02 13:56:00 -0500

    ok test main root

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import Flask, flash, reidrect, render_template, request, session
-from flask_session import mkdtemp
+from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 import sqlite3
 from functools import wraps
