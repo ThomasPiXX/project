2afe43f85e2d6c6e271121212d8b7aef7e085089
Tom Pivin <tommy.pivin94@gmail.com>
2023-03-02 11:37:21 -0500

    changing Database browser , squleton in progress

--- app.py
+++ app.py
@@ -2,8 +2,104 @@ from datetime import datetime
 from flask import Flask, flash, reidrect, render_template, request, session
 from flask_session import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+from functools import wraps
 
-
+#configure flask
 app = Flask(__name__)
 
-db= 
+
+#connect database
+db= sqlite3.connect('where.db')
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#def login require
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    https://flask.palletsprojects.com/en/1.1.x/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+
+
+#register route
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    # Get the form data
+    if request.method == "POST":
+        username= request.form.get('username')
+        password = request.form.get('password')
+        confirm_password = request.form.get('confirmation')
+
+      #check if any required field are empty
+
+        if not username or not password or not confirm_password:
+            return apology('required field empty')
+
+        #password match
+
+        if password != confirm_password:
+           return apology('password dont match')
+
+        #hashing the password
+
+        hash = generate_password_hash(password)
+
+        try:
+            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+        except:
+            return apology("Username already exist")
+        # Display a message upon successful registration
+
+        flash('You are now registered and can log in!', 'success')
+        return redirect('/login')
+
+    else:
+        return render_template("register.html")
+    
+    #login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
